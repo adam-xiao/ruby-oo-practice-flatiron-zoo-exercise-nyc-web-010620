033133113ef8b495e751cbc18912ae6552310fee
github username <github email address>
2020-01-13 14:15:30 -0500

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,50 @@
 class Zoo
 
+    attr_reader :location
+    attr_accessor :name
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        #@animals = [], dont do - need single source of truth, IN THIS CASE ANIMAL(its usually the many)
+
+        @@all << self
+    end
+
+    def add_animal(species, weight, nickname)
+        #If being extra do an if/else to see if it exists already. The below would be the else
+        x = Animal.all.find { |animal| animal.nickname == nickname }
+
+        if x   
+            x.zoo = self
+        else
+            Animal.new(species, weight, nickname, self)
+        end
+    end
+
+    def animals
+        Animal.all.select { |animal| animal.zoo == self }
+    end
+
+    def animal_species
+        animals.map { |animal| animal.species }.uniq
+    end
+
+    def find_by_species(specie)
+        animals.select { |animal| animal.species == specie}
+    end
+
+    def animal_nicknames
+        animals.map { |animal| animal.nickname }
+    end
+    
+    def self.find_by_location(location)
+        self.all.select { |zoo| zoo.location == location }
+    end
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,15 @@ require 'pry'
 
 #Test your code here
 
+zoo1 = Zoo.new("ABC Zoo", "Philly")
+zoo2 = Zoo.new("XYZ Zoo", "NYC")
+zoo3 = Zoo.new("MNO Zoo", "NYC")
+
+anim1 = Animal.new("Cat", 15, "Kitty")
+anim2 = Animal.new("Dog", 25, "Doggy")
+anim3 = Animal.new("Cat", 18, "Kit")
+anim4 = Animal.new("Cat", 13, "Catty")
+
 
 binding.pry
 puts "done"
